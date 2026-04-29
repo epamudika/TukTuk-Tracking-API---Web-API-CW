1d374f21715f0b05612f2c2a9c4c6d9ce284801c
epamudika <erandi201326@gmail.com>
2026-04-29 21:58:47 +0530

    Create controller files - regards to the MVC architecture

--- routes/auth.js
+++ routes/auth.js
@@ -1,77 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const authController = require('../controllers/auth.controller');
 
-router.post('/register', async (req, res) =>{
-    try{
-        const {username, password, role, province, district} = req.body;
-        //Check if username already exists
-        const existing = await User.findOne({username});
-        if (existing){
-            return res.status(400).json({
-                message: 'Username already exists'
-            });
-        }
-        // Hash password before saving
-        const hashed = await bcrypt.hash(password,10);
-
-        //Create new user
-        const user = new User({
-            username,
-            password: hashed,
-            role,
-            province,
-            district
-        });
-        await user.save();
-        res.status(201).json({message: 'User created successfully'});
-    }
-    catch (err){
-        res.status(500).json({message: err.message});
-    }
-} );
-
-//Logi user
-router.post('/login', async(req, res) =>{
-    try{
-        const {username, password} = req.body;
-
-        //Find user by username
-        const user = await User.findOne({username});
-        if (!user){
-            return res.status(404).json({message: 'User not Found'});
-        }
-        
-        //Check password
-        const match = await bcrypt.compare(password, user.password);
-        if (!match){
-            return res.status(400).json({message: 'Wrong password'});
-        }
-
-        //Create JWT token
-        const token = jwt.sign(
-            {
-                id: user._id,
-                role: user.role,
-                province: user.province,
-                district: user.district
-            },
-            process.env.JWT_SECRET,
-            {expiresIn: '24h'}
-        );
-
-        res.status(200).json({
-            message: 'Login successful',
-            token,
-            role: user.role
-        });
-        
-    }catch (err){
-        res.status(500).json({message: err.message});
-    }
-});
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
 
