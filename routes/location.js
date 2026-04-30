af61e771837a410d826ed33108a282f6fb6c77a8
epamudika <erandi201326@gmail.com>
2026-05-01 01:57:55 +0530

    Update the location-routes file

--- routes/location.js
+++ routes/location.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
+const locationController = require('../controllers/location.controller');
+const { protect, officerOrAdmin } = require('../middleware/authMiddleware');
 
-router.post('/register', authController.register);
-router.post('Login', authController.Login);
+/**
+ * @swagger
+ * /api/locations/ping:
+ *   post:
+ *     summary: Send GPS location ping
+ *     tags: [Locations]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               tukTukId:
+ *                 type: string
+ *               latitude:
+ *                 type: number
+ *               longitude:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Location saved
+ */
+
+router.post('/ping', protect, locationController.pingLocation);
+router.get('/live', protect, officerOrAdmin, locationController.getLiveLocations);
+router.get('/history/:tukTukId', protect, officerOrAdmin, locationController.getLocationHistory);
 
 module.exports = router;
